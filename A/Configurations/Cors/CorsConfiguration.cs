cdc8cd9efe5fad761f83d05822601160578dfad3
KoltIP <kolt.offwork@gmail.com>
2022-10-28 07:24:58 +0300

    add cors

---
+++ A/Configurations/Cors/CorsConfiguration.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace A.Configurations.Cors
+{
+    public static class CorsConfiguration
+    {
+        private static string CorsName = "myAllowSpecificOrigins";
+        private static string AllowedHosts = "http://localhost:5031";
+
+
+        public static IServiceCollection AddAppCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: CorsName,
+                                  policy =>
+                                  {
+                                      policy.WithOrigins(AllowedHosts);
+                                  });
+            });
+
+            return services;
+        }
+
+        public static void UseAppCors(this IApplicationBuilder app)
+        {
+            app.UseCors(CorsName);
+        }
+    }
+}
--- A/Program.cs
+++ A/Program.cs
@@ -1,10 +1,13 @@
 using A;
+using A.Configurations.Cors;
 using A.Data.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +20,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+services.AddAppCors();
 
 services.AddAppServices();
 
@@ -28,6 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseAppCors();
 
 app.UseAuthorization();
 
